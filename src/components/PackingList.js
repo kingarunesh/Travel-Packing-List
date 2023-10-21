3389ed913b450b34f738d0fabfb9a166b1ce8188
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-10-21 07:43:58 +0530

    4.1 : sorting

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 import Item from "./Item";
 
 export default function PackingList({ items, onPackedHandler, onDeleteHandler, onClearHandler }) {
+    const [sort, setSort] = useState("input");
+
+    let sortedItems;
+
+    //  input sort
+    if (sort === "input") sortedItems = items;
+
+    //  description sort
+    if (sort === "description") {
+        const result = items.slice().sort((a, b) => a.title.localeCompare(b.title));
+        sortedItems = result;
+    }
+
+    //  packed sort
+    if (sort === "packed") {
+        const result = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+        sortedItems = result;
+    }
+
     return (
         <>
             <main className="conatiner__main">
                 <section className="conatiner__main--items">
-                    {items.map((item) => (
+                    {sortedItems.map((item) => (
                         <Item item={item} onPackedHandler={onPackedHandler} onDeleteHandler={onDeleteHandler} key={item.id} />
                     ))}
                 </section>
 
                 <section className="conatiner__main--filter">
-                    <select>
+                    <select value={sort} onChange={(e) => setSort(e.target.value)}>
                         <option value="input">sort by input order</option>
-                        <option value="descrition">sort by descrition</option>
+                        <option value="description">sort by descrition</option>
                         <option value="packed">sort by packed item</option>
                     </select>
 
